b7d2df871ccc21e2d243878dadce31f7c39e06e2
emrekas <y.emrekas@outlook.com>
2022-01-05 18:42:02 +0300

    station model different version

--- src/models/indego.model.ts
+++ src/models/indego.model.ts
@@ -1,61 +1,54 @@
 import { model, Schema, Document } from 'mongoose';
 import { Indego } from '@interfaces/indego.interface';
 
-const bikeSchema: Schema = new Schema({
-  dockNumber: Number,
-  isElectric: Boolean,
-  isAvailable: Boolean,
-  battery: Number,
-});
-
-const propertiesSchema: Schema = new Schema({
-  id: Number,
-  name: String,
-  coordinates: [Number],
-  totalDocks: Number,
-  docksAvailable: Number,
-  bikesAvailable: Number,
-  classicBikesAvailable: Number,
-  smartBikesAvailable: Number,
-  electricBikesAvailable: Number,
-  rewardBikesAvailable: Number,
-  rewardDocksAvailable: Number,
-  kioskStatus: String,
-  kioskPublicStatus: String,
-  kioskConnectionStatus: String,
-  kioskType: Number,
-  addressCity: String,
-  addressState: String,
-  addressZipCode: String,
-  bikes: [bikeSchema],
-  closeTime: null,
-  eventEnd: null,
-  eventStart: null,
-  isEventBased: Boolean,
-  isVirtual: Boolean,
-  kioskId: Number,
-  notes: null,
-  openTime: null,
-  publicText: String,
-  timeZone: null,
-  trikesAvailable: Number,
-  latitude: Number,
-  longitude: Number,
-});
-
-const geometrySchema: Schema = new Schema({
-  coordinates: [Number],
-  type: String,
-});
-
-const featureSchema = new Schema({
-  geometry: geometrySchema,
-  properties: propertiesSchema,
-  type: String,
-});
-
 const indegoSchema: Schema = new Schema({
-  features: [featureSchema],
+  features: [
+    {
+      geometry: { coordinates: [Number], type: String },
+      properties: {
+        id: Number,
+        name: String,
+        coordinates: [Number],
+        totalDocks: Number,
+        docksAvailable: Number,
+        bikesAvailable: Number,
+        classicBikesAvailable: Number,
+        smartBikesAvailable: Number,
+        electricBikesAvailable: Number,
+        rewardBikesAvailable: Number,
+        rewardDocksAvailable: Number,
+        kioskStatus: String,
+        kioskPublicStatus: String,
+        kioskConnectionStatus: String,
+        kioskType: Number,
+        addressCity: String,
+        addressState: String,
+        addressZipCode: String,
+        bikes: [
+          {
+            dockNumber: Number,
+            isElectric: Boolean,
+            isAvailable: Boolean,
+            battery: Number,
+          },
+        ],
+        closeTime: null,
+        eventEnd: null,
+        eventStart: null,
+        isEventBased: Boolean,
+        isVirtual: Boolean,
+        kioskId: Number,
+        notes: null,
+        openTime: null,
+        publicText: String,
+        timeZone: null,
+        trikesAvailable: Number,
+        latitude: Number,
+        longitude: Number,
+      },
+      type: String,
+    },
+  ],
   type: String,
 });
 
--- src/services/indego.service.ts
+++ src/services/indego.service.ts
@@ -6,14 +6,160 @@ import { Indego } from '@/interfaces/indego.interface';
 import axios from 'axios';
 
 class IndegoService {
-  // public indegos = indegoModel;
+  public indegos = indegoModel;
 
   public async syncIndegoData() {
     try {
-      const indegoData: Indego = await axios.get('https://www.rideindego.com/stations/json/');
-      console.log(indegoData);
+      // const indegoData = await axios.get('https://www.rideindego.com/stations/json/');
+      // console.log(indegoData.data);
 
-      // this.indegos.create(indegoData);
+      const indego: any = JSON.parse(`{
+        "features": [
+          {
+            "geometry": {
+              "coordinates": [
+                -75.16374,
+                39.95378
+              ],
+              "type": "Point"
+            },
+            "properties": {
+              "id": 3004,
+              "name": "Municipal Services Building Plaza",
+              "coordinates": [
+                -75.16374,
+                39.95378
+              ],
+              "totalDocks": 30,
+              "docksAvailable": 15,
+              "bikesAvailable": 15,
+              "classicBikesAvailable": 12,
+              "smartBikesAvailable": 0,
+              "electricBikesAvailable": 3,
+              "rewardBikesAvailable": 15,
+              "rewardDocksAvailable": 15,
+              "kioskStatus": "FullService",
+              "kioskPublicStatus": "Active",
+              "kioskConnectionStatus": "Active",
+              "kioskType": 1,
+              "addressStreet": "1401 John F. Kennedy Blvd.",
+              "addressCity": "Philadelphia",
+              "addressState": "PA",
+              "addressZipCode": "19102",
+              "bikes": [
+                {
+                  "dockNumber": 3,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                },
+                {
+                  "dockNumber": 13,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                },
+                {
+                  "dockNumber": 16,
+                  "isElectric": true,
+                  "isAvailable": true,
+                  "battery": 0.8
+                },
+                {
+                  "dockNumber": 17,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                },
+                {
+                  "dockNumber": 20,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                },
+                {
+                  "dockNumber": 21,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                },
+                {
+                  "dockNumber": 22,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                },
+                {
+                  "dockNumber": 23,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                },
+                {
+                  "dockNumber": 24,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                },
+                {
+                  "dockNumber": 25,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                },
+                {
+                  "dockNumber": 26,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                },
+                {
+                  "dockNumber": 27,
+                  "isElectric": true,
+                  "isAvailable": true,
+                  "battery": 0.8
+                },
+                {
+                  "dockNumber": 28,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                },
+                {
+                  "dockNumber": 29,
+                  "isElectric": true,
+                  "isAvailable": true,
+                  "battery": 0.8
+                },
+                {
+                  "dockNumber": 30,
+                  "isElectric": false,
+                  "isAvailable": true,
+                  "battery": null
+                }
+              ],
+              "closeTime": null,
+              "eventEnd": null,
+              "eventStart": null,
+              "isEventBased": false,
+              "isVirtual": false,
+              "kioskId": 3004,
+              "notes": null,
+              "openTime": null,
+              "publicText": "",
+              "timeZone": null,
+              "trikesAvailable": 0,
+              "latitude": 39.95378,
+              "longitude": -75.16374
+            },
+            "type": "Feature"
+          }
+        ],
+        "type": "asd"
+      }`);
+      console.log(indego);
+
+      this.indegos.create(indego);
     } catch (e) {
       console.log(e);
     }
